51355e2ff008c78d155031b03cf0ab95906e41bb
ZiqubuLebo <lebogangmoesha@gmail.com>
2026-06-05 10:37:41 +0200

    just space

--- cadence_app/app.py
+++ cadence_app/app.py
@@ -18,7 +18,7 @@ def allowed_file(filename):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html')  
 
 @app.route('/upload', methods=['POST'])
 def upload():
